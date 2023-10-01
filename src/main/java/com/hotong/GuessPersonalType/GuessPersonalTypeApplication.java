77d660ac52f4ed459f261696e33632ea7ae10a8a
hachanghyun <haccwx@naver.com>
2023-10-02 08:00:49 +0900

    first commit

---
+++ src/main/java/com/hotong/GuessPersonalType/GuessPersonalTypeApplication.java
@@ -0,0 +1,13 @@
+package com.hotong.GuessPersonalType;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessPersonalTypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessPersonalTypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotong/GuessPersonalType/GuessPersonalTypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotong.GuessPersonalType;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessPersonalTypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
